9209aa2b72ed15ecb755a6ade24d07ee6d636731
Alexandra Horkina <al.horkina@gmail.com>
2019-03-08 10:59:13 +0200

    add slider

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,14 @@
-let add = (a,b) => a+b
-console.log(add(2,6))
+(function ($) {
+    $(document).ready(function () {
+        $('.hero-slider').slick({
+            arrows: false,
+            dots: true
+        });
+
+        $('.portfolio-slider').slick({
+            arrows: true,
+            dots: false
+        });
+
+    })
+})(jQuery);
